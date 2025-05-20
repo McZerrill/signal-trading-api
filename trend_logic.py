b4af52998bb5ccf6f3dae5b1e78dbf7b5d84508c
McZerrill <dinozerrilli@gmail.com>
2025-05-20 19:50:35 +0200

    Update trend_logic.py

--- trend_logic.py
+++ trend_logic.py
@@ -118,7 +118,7 @@ def analizza_trend(hist: pd.DataFrame):
         (penultimo['EMA_7'] < penultimo['EMA_25'] < penultimo['EMA_99']
          and trend_up and dist_diff > 0 and rsi > 56 and macd > macd_signal 
          and macd > 0.001 and 2 <= candele_trend_up <= 6) 
-        or (trend_up and candele_trend_up in [3, 4] and rsi > 56 and macd > macd_signal and dist_diff > 0)
+        or (trend_up and candele_trend_up in range(2, 7) and rsi > 56 and macd > macd_signal and dist_diff > 0)
     )
 
     if condizioni_buy:
@@ -137,7 +137,7 @@ def analizza_trend(hist: pd.DataFrame):
         (penultimo['EMA_7'] > penultimo['EMA_25'] > penultimo['EMA_99']
          and trend_down and dist_diff > 0 and rsi < 44 and macd < macd_signal 
          and macd < -0.001 and 2 <= candele_trend_down <= 6) 
-        or (trend_down and candele_trend_down in [3, 4] and rsi < 44 and macd < macd_signal and dist_diff > 0)
+        or (trend_down and candele_trend_down in range(2, 7) and rsi < 44 and macd < macd_signal and dist_diff > 0)
     )
 
     if condizioni_sell:
